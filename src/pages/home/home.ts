f92e1a68e257765db6dbc97a0b8e681a45166298
shoppinglink <shoppinglink@163.com>
2017-12-15 08:08:04 +0800

    1、增加上拉加载更多

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,16 +10,27 @@ import { Strings }from '../../providers/strings';
 })
 export class HomePage {
     jokeList:any;
+    currentPage:number=1;
 
   constructor(public navCtrl: NavController,private httpService:HttpService) {
     this.initData();
   }
 
   initData(){
-      this.httpService.mobPostJoke(1).then(list=>{
+      this.httpService.mobPostJoke(this.currentPage).then(list=>{
           console.log(list);
           this.jokeList = list;
       });          
+  }
+
+  doInfinite(infiniteScroll){
+    this.currentPage++;
+    this.httpService.mobPostJoke(this.currentPage).then(list=>{
+      for(let i=0;i<Object.keys(list).length;i++){
+        this.jokeList.push(list[i]);
+      };
+      infiniteScroll.complete();
+    })
   }
 
 }
