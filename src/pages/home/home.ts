39abdc8e7f278415a4eac9097baf9d4579fbe24c
K8008\linshaoping <shoppinglink@163.com>
2017-12-15 17:31:40 +0800

    1、去掉home的ion-header

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,66 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { HttpService } from '../../providers/httpService';
 import { Strings }from '../../providers/strings';
+import { Content } from 'ionic-angular/components/content/content';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    @ViewChild(Content) content:Content;
     jokeList:any;
     currentPage:number=1;
+    joke:string='txt';
+    selectedSegment:boolean = false;
 
   constructor(public navCtrl: NavController,private httpService:HttpService) {
     this.initData();
   }
 
   initData(){
-      this.httpService.mobPostJoke(this.currentPage).then(list=>{
-          console.log(list);
+      console.log('init');
+      this.httpService.mobPostJoke(this.currentPage,this.joke).then(list=>{
           this.jokeList = list;
       });          
   }
 
-  doInfinite(infiniteScroll){
-    this.currentPage++;
-    this.httpService.mobPostJoke(this.currentPage).then(list=>{
-      for(let i=0;i<Object.keys(list).length;i++){
-        this.jokeList.push(list[i]);
-      };
-      infiniteScroll.complete();
-    })
-  }
+  onSelectSegment(value){
+      console.log('select');
+      if(value==this.joke){
+            this.content.scrollToTop();
+          return;
+      }
+      this.currentPage = 1;
+    this.content.scrollToTop(); 
+    this.httpService.mobPostJoke(this.currentPage,value).then(list=>{
+        this.jokeList = list;
+    });
+    }
 
+  doInfinite(infiniteScroll){
+        this.currentPage++;
+        this.httpService.mobPostJoke(this.currentPage,this.joke).then(list=>{
+            for(let i=0;i<Object.keys(list).length;i++){
+              this.jokeList.push(list[i]);
+            };
+            infiniteScroll.complete();
+          }).catch(()=>{
+              this.currentPage--;
+              infiniteScroll.complete;
+          })
+      }
 }
+
+//visual studio code 设置tab为4个字符
+/**
+ * 
+    {
+        "editor.tabSize": 4,
+        "editor.detectIndentation": false,
+        "files.autoSave": "off"
+    }
+ * 
+ */
--- src/providers/httpService.ts
+++ src/providers/httpService.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Strings } from '../providers/strings';
 import { Http,Headers } from '@angular/http';
+import { LoadingController } from "ionic-angular/components/loading/loading-controller";
 
 @Injectable()
 export class HttpService{
-    constructor(private strings:Strings,private http:Http){}
+    constructor(private strings:Strings,private http:Http,private loadingCtrl:LoadingController){}
 
     mobGetHistory(day){
         let url = this.strings.mob.history_url+'?key='+this.strings.mob.key+'&day='+day;
@@ -37,19 +38,35 @@ export class HttpService{
         
     }
 
-    mobPostJoke(page){
-        let url = this.strings.yy.joke_txt_url;
+    mobPostJoke(page,type){
+        let url;
+        if(type=='txt'){
+            url = this.strings.yy.joke_txt_url;
+        }else if(type =='img'){
+            url = this.strings.yy.joke_img_url;
+        }else if(type =='gif'){
+            url = this.strings.yy.joke_gif_url;
+        }else{
+            url = this.strings.yy.joke_txt_url;
+        }
         let prama = {showapi_appid:this.strings.yy.appid,showapi_sign:this.strings.yy.secret,
             page:page,maxResult:this.strings.yy.max_result}
         let header = new Headers();
         header.append('Content-Type','application/x-www-form-urlencoded');
         return new Promise((resolve,reject)=>{
+            let loading = this.loadingCtrl.create({
+                content:'加载中...',
+            });
+            loading.present();
             this.http.post(url,this.objectTransForm(prama),{headers:header}).subscribe(response=>{
                 let list =response.json().showapi_res_body.contentlist;
                 resolve(list);
+                console.log(list); 
+                loading.dismiss();               
             },error=>{
                 console.log(error);
                 reject(error);
+                loading.dismiss();
             })
         })
     }
